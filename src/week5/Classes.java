0033b8c6549ddb67095cd540a3d4c30c8878ba1e
f-guedes <fernando.hpg2@gmail.com>
2022-10-09 01:17:52 -0400

    OOP concenpts: classes and encapsulation

---
+++ src/week5/Classes.java
@@ -0,0 +1,22 @@
+package week5;
+
+public class Classes {
+
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.setFirstName("Tony");
+		student1.setLastName("Hawk");
+		student1.setGradeLevel(12);
+		student1.setPhoneNumber("617-457-1836");
+		
+		student1.introduce();
+		
+		Student student2 = new Student("Bob", "Burnquist");
+		student2.introduce();
+		
+		Student student3 = new Student ("Bucky", "Lasek", "617-786-4570", 10);
+		student3.introduce();
+
+	}
+
+}
---
+++ src/week5/Encapsulation.java
@@ -0,0 +1,28 @@
+package week5;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Access modifiers:
+		 * public					accessible everywhere
+		 * private					only accessible within the class itself
+		 * protected				accessible within the class or other classes in the same package and subclasses
+		 * default (No modifier)	same as protected, but not accessible in subclasses in a different package
+		 */
+		
+		Student student = new Student();
+		student.setFirstName("Jack");
+		student.setLastName("Tequila");
+		student.setPhoneNumber("6173451792");
+		System.out.println(student.getFirstName() + " " + student.getLastName());
+		System.out.println(student.getPhoneNumber());
+		
+		Rectangle rec = new Rectangle(10.0, 15.0);
+		rec.setLength(10.0);
+		System.out.println(rec.getArea());
+
+	}
+
+}
